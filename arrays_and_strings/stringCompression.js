2805ab970144e758a64ce7d81b06edba2f3c0f8e
kithaca <kithaca@gmail.com>
2016-04-26 18:25:49 -0700

    stringCompression

--- arrays_and_strings/stringCompression.js
+++ arrays_and_strings/stringCompression.js
@@ -8,3 +8,24 @@ output: a2b1c5a3
 
 */
 
+var compressString = function (string) {
+	var len = string.length;
+	var current = string[0];
+	var curr_length = 1;
+	var compressed = "";
+	for (var i = 1; i < len; i++) {
+		if (string[i] === current) {
+			curr_length++;
+		} else {
+			compressed = compressed + current + curr_length;
+			curr_length = 1;
+		}
+		current = string[i];
+	}
+	compressed = compressed + current + curr_length;
+	return compressed.length < len ? compressed : string;
+};
+
+console.log(compressString("aabcccccaaa"));
+console.log(compressString("aabcccccaaabc"));
+console.log(compressString("aabcccccaaabcdjklm"));
