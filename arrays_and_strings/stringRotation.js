63c99b80ecc2e46a96b65141d41dd80014f29f87
kithaca <kithaca@gmail.com>
2016-04-27 23:34:09 -0700

    stringRotation

--- arrays_and_strings/stringRotation.js
+++ arrays_and_strings/stringRotation.js
@@ -1,2 +1,14 @@
 /* Given two strings, check if one is a rotation of the other. Assume
 you have a method isSubstring that you can call one time only. */
+
+var isSubstring = function (str, substr) {
+	return (new RegExp(substr).test(str));
+};
+
+var stringRotation = function (original, rotated) {
+	var doubled = rotated + rotated;
+	return isSubstring(doubled, original) ? true : false;
+};
+
+console.log(stringRotation("cat", "tca"));
+console.log(stringRotation("horse", "ohser"));
